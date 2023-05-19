68c066130641fa728c7cb0b0a90203dc4abfe7f3
GustavoFernandes <gustavo.ribeiro.fernandes1@gmail.com>
2023-05-19 20:01:03 -0300

    feat: :sparkles: including Course controller, model and repository

--- src/main/java/com/gustavo/crudspring/controller/CourseController.java
+++ src/main/java/com/gustavo/crudspring/controller/CourseController.java
@@ -1,15 +1,19 @@
 package com.gustavo.crudspring.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gustavo.crudspring.model.Course;
+
 @RestController
-@RequestMapping("/api/hello")
-public class HelloController {
+@RequestMapping("/api/courses")
+public class CourseController {
 
   @GetMapping
-  public String hello() {
-    return "Hello, World!";
+  public List<Course> list() {
+    return null;
   }
 }
