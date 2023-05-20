3e309147cade12710ef3774d6b7d9540f9557848
GustavoFernandes <gustavo.ribeiro.fernandes1@gmail.com>
2023-05-20 11:09:52 -0300

    feat: :sparkles: implementing findAll in controller

--- src/main/java/com/gustavo/crudspring/controller/CourseController.java
+++ src/main/java/com/gustavo/crudspring/controller/CourseController.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gustavo.crudspring.model.Course;
+import com.gustavo.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourseController {
+
+  private final CourseRepository courseRepository;
 
   @GetMapping
   public List<Course> list() {
-    return null;
+    return courseRepository.findAll();
   }
 }
