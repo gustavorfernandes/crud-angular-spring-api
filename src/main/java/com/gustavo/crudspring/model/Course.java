6d53a587e06fc3552a68748160d93acba70aa0a3
GustavoFernandes <gustavo.ribeiro.fernandes1@gmail.com>
2023-05-27 10:25:31 -0300

    feat: :sparkles: including post method at course controller

--- src/main/java/com/gustavo/crudspring/model/Course.java
+++ src/main/java/com/gustavo/crudspring/model/Course.java
@@ -1,10 +1,13 @@
 package com.gustavo.crudspring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import lombok.Data;
 
 @Data
@@ -13,6 +16,7 @@ public class Course {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @JsonProperty("_id")
   private Long id;
 
   @Column(length = 200, nullable = false)
