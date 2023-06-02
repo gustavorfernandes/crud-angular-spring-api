20049c602b6950cb4ed6d853d7c7a5acfe595a51
Gustavo <gustavo.ribeiro.fernandes1@gmail.com.br>
2023-06-02 09:03:28 -0300

    feat: :sparkles: adding put method in course controller

--- src/main/java/com/gustavo/crudspring/controller/CourseController.java
+++ src/main/java/com/gustavo/crudspring/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -32,7 +33,7 @@ public class CourseController {
   @GetMapping("/{id}")
   public ResponseEntity<Course> registry(@PathVariable Long id) {
     return courseRepository.findById(id)
-        .map(data -> ResponseEntity.ok().body(data))
+        .map(foundedData -> ResponseEntity.ok().body(foundedData))
         .orElse(ResponseEntity.notFound().build());
   }
 
@@ -40,5 +41,17 @@ public class CourseController {
   @ResponseStatus(code = HttpStatus.CREATED)
   public Course create(@RequestBody Course registry) {
     return courseRepository.save(registry);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course registry) {
+    return courseRepository.findById(id)
+        .map(foundedData -> {
+          foundedData.setName(registry.getName());
+          foundedData.setCategory(registry.getCategory());
+          Course updatedRegistry = courseRepository.save(foundedData);
+          return ResponseEntity.ok().body(updatedRegistry);
+        })
+        .orElse(ResponseEntity.notFound().build());
   }
 }
